371b33ef64f12aa5117a2764fb5e161af53a5076
senramya <119154580+senramya@users.noreply.github.com>
2025-03-17 11:53:02 +0530

    Create Ex.java

---
+++ Ex.java
@@ -0,0 +1,19 @@
+
+class pre
+{
+
+public static void main(String[]args)
+{
+
+int a=10;
+int b=12;
+
+b=b + a++;
+int c= ++a +b;
+
+ System.out.println(a);
+ System.out.println(b);
+ System.out.println(c);
+ 
+ }
+}
